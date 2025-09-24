96eef8634b4e64fe70e2c77185ae41c74ee9b06f
gpBlockchain <744158715@qq.com>
2025-09-24 17:50:53 +0800

    add udt json

--- prepare.sh
+++ prepare.sh
@@ -12,6 +12,6 @@ offckb node > node.log 2>&1 &
 # 启动 ckb-udt-convert-service
 cp ../.env.dev .env
 cp ../devnet-offckb.json  .
-cp ../udts.json.sample .
+cp ../udts.json.sample udts.json
 node dist/entries/singlesig_all.js > ckb-udt-convert-service.log 2>&1 &
 cd ../
